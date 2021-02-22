73a44b3ec16f16918307c6a4bb25f5653eb6b7de
Ahmet Emin KARA <emin9457@gmail.com>
2021-02-22 22:11:14 +0300

    animasyonu çalışıyor, paramterler düzenlenecek

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -11,35 +11,26 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: ThemeData.dark(),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
   final int durationSecond = 5;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        centerTitle: true,
+        title: Text("Legend Toastr"),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
@@ -47,71 +38,77 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.end,
           children: <Widget>[
-            FlatButton(
+            OutlineButton(
+                borderSide: BorderSide(
+                    color: LegendToastr.getColor(Style.SUCCESS).bgColor),
                 child: Text("SUCCESS"),
                 onPressed: () {
-                  _counter++;
                   final builder = LegendToastrBuilder(context)
-                    ..setMessage("SUCCESS $_counter")
+                    ..setMessage("SUCCESS MESSAGE")
                     ..setTheme(Style.SUCCESS)
                     ..setPadding(EdgeInsets.zero)
                     ..setDuration(durationSecond);
 
                   LegendToastr.show(builder);
                 }),
-            FlatButton(
+            OutlineButton(
+                borderSide: BorderSide(
+                    color: LegendToastr.getColor(Style.DANGER).bgColor),
                 child: Text("DANGER"),
                 onPressed: () {
-                  _counter++;
                   final builder = LegendToastrBuilder(context)
-                    ..setMessage("DANGER $_counter")
+                    ..setMessage("DANGER MESSAGE")
                     ..setTheme(Style.DANGER)
                     ..setPadding(EdgeInsets.zero)
                     ..setDuration(durationSecond);
 
                   LegendToastr.show(builder);
                 }),
-            FlatButton(
+            OutlineButton(
+                borderSide: BorderSide(
+                    color: LegendToastr.getColor(Style.WARNING).bgColor),
                 child: Text("WARNING"),
                 onPressed: () {
-                  _counter++;
                   final builder = LegendToastrBuilder(context)
-                    ..setMessage("WARNING $_counter")
+                    ..setMessage("WARNING MESSAGE")
                     ..setTheme(Style.WARNING)
                     ..setPadding(EdgeInsets.zero)
                     ..setDuration(durationSecond);
                   LegendToastr.show(builder);
                 }),
-            FlatButton(
+            OutlineButton(
+                borderSide: BorderSide(
+                    color: LegendToastr.getColor(Style.INFO).bgColor),
                 child: Text("INFO"),
                 onPressed: () {
-                  _counter++;
                   final builder = LegendToastrBuilder(context)
-                    ..setMessage("INFO $_counter")
+                    ..setMessage("INFO MESSAGE")
                     ..setTheme(Style.INFO)
                     ..setPadding(EdgeInsets.zero)
                     ..setDuration(durationSecond);
 
                   LegendToastr.show(builder);
                 }),
-            FlatButton(
+            OutlineButton(
+                borderSide: BorderSide(
+                    color: LegendToastr.getColor(Style.LIGHT).bgColor),
                 child: Text("LIGHT"),
                 onPressed: () {
-                  _counter++;
                   final builder = LegendToastrBuilder(context)
-                    ..setMessage("LIGHT $_counter")
+                    ..setMessage("LIGHT MESSAGE")
                     ..setTheme(Style.LIGHT)
                     ..setPadding(EdgeInsets.zero)
                     ..setDuration(durationSecond);
 
                   LegendToastr.show(builder);
                 }),
-            FlatButton(
+            OutlineButton(
+                borderSide: BorderSide(
+                    color: LegendToastr.getColor(Style.DARK).bgColor),
                 child: Text("DARK"),
                 onPressed: () {
-                  _counter++;
                   final builder = LegendToastrBuilder(context)
-                    ..setMessage("DARK $_counter")
+                    ..setMessage("DARK MESSAGE")
                     ..setTheme(Style.DARK)
                     ..setPadding(EdgeInsets.zero)
                     ..setDuration(durationSecond);
